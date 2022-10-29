f7b5f05b52cbabab264ff6a831eec3cf0ab2e07b
Arivazhagan <arivu.akil@yahoo.com>
2022-10-29 18:00:49 +0530

    Today's Work

---
+++ Sample.java
@@ -0,0 +1,33 @@
+public class Sample
+{
+public static void main (String [] args)
+{
+
+System.out.println ("My first program");
+}
+}
+
+
+1. public class ControlStatement {
+public static void main(String[] args) {
+int a=25;
+if(a>25)
+a++;
+a+=a;
+System.out.print(a);
+}
+} 
+
+
+public class Test 
+{
+public static void main(String[] args) 
+{
+int a=25;
+if(a>10)
+System.out.print("ok");
+else if(true);
+System.out.print("bye");
+}
+}
+
