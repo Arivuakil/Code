0f770476f281088e81cb3d9e7d16262bc5c871b9
Arivazhagan M <93433897+Arivuakil@users.noreply.github.com>
2022-11-01 13:56:40 +0530

    Add files via upload

---
+++ Arivazhagan.java
@@ -0,0 +1,20 @@
+public stactic void main (String{} args)
+{
+ArrayList al = new ArrayList();
+al.add(10);
+al.add(5);
+al.add(7);
+al.add(10);
+al.add(4);
+System.out.println(al);
+
+int size = al.size();
+System.out.println(size);
+al.add(size/2,111);
+System.out.println(al);
+System.out.println(al.get(3);
+				   
+al.remove(4);
+System.out.println(al.indexof(7));
+System.out.println(al.indexof(10));
+System.out.println(al.lastIndexof(10));			   
